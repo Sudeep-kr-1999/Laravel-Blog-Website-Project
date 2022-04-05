6241add0fd2faae4c3f8c82adc680abd75fc320c
Sudeep Kumar <sudeepagrawal1999@gmail.com>
2022-04-05 23:40:41 +0530

    Home page modified

---
+++ resources/views/blog/about.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+    <div class="w-4/5 m-auto text-left">
+        <div class="py-15">
+            <h1 class="text-6xl ">
+                Blog Web Website in Laravel Framework
+            </h1>
+        </div>
+        <div class="py-15">
+            <h2 class="text-5xl ">
+              We Value Your Thought and Innovation
+            </h2>
+        </div>
+        <div class="py-15">
+            <h3 class="text-4xl ">
+               We provide a way to share your thought to the world
+            </h3>
+        </div>
+        <div class="py-15">
+            <h4 class="text-3xl ">
+               Provide any suggestion you feel, We will be more than happy to hear from you.
+            </h4>
+        </div>
+    </div>
+
+
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center ">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                    WELCOME TO BLOG WEB
                 </h1>
                 <a href="/blog" class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">Read
                     More</a>
@@ -14,36 +14,36 @@
 
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://cdn.pixabay.com/photo/2018/03/10/18/03/laptop-3214756_960_720.png" alt="" width="700">
+            <img src="https://images.unsplash.com/photo-1546074177-ffdda98d214f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="" width="700" style="box-shadow: 10px">
         </div>
         <div class="m-auto sm:m-auto text-left w-4/5 block">
             <h2 class="text-4xl font-extrabold text-gray-600">
-                Struggling to be a better webdeveloper
+                Create Your First Blog
             </h2>
             <p class="py-8 text-gray-500 text-l">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit corporis doloribus natus blanditiis reiciendis harum aspernatur nam soluta sed, atque laborum sit quae deserunt! Excepturi voluptas quos obcaecati totam, debitis aperiam? Magni reiciendis reprehenderit doloremque dicta est. Commodi autem animi eligendi ad illum ipsa fugit reprehenderit, ut vitae eum sequi voluptates at perferendis! Ullam aut eligendi non voluptatum.
+                Share your thought and let other learn from your Experience.
             </p>
             <p class="font-extrabold text-gray-600 text-l pb-9">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores assumenda praesentium, velit fugiat itaque maiores nulla, obcaecati minima suscipit officia, adipisci earum esse non optio animi aliquam rem ad fugit? Quae quam dolorum architecto. Hic atque rerum ratione aliquam cum magnam sequi voluptate aut enim animi, officia totam eaque commodi reprehenderit blanditiis, dolore iste quod nisi similique. Et!
+                Welcome to the blogWeb. A platform to share thoughts, emotions, knowledge and do something great for other for good.
             </p>
-            <a href="/blog" class="uppercase bg-blue-500 text-gray-100 text-sm font-extrabold py-3 px-8 rounded-3xl">Find out More</a>
+            <a href="/blog" class="uppercase bg-blue-500 text-gray-100 text-sm font-extrabold py-3 px-8 rounded-3xl">Visit Blog</a>
         </div>
     </div>
     <div class="text-center p-15 bg-black text-white">
         <h2 class="text-2xl pb-5 text-l">
-            Im expert .............
+           BlogWeb
         </h2>
         <span class="font-extrabold block text-4xl py-1">
-            UX Design
+            Technical Blog
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Project Mangement
+            Social Blog
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Digital Stratagy
+            Sports Blog
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Backend development
+            Cinemas Blog
         </span>
     </div>
     <div class="text-center py-15">
@@ -52,17 +52,17 @@
             Recent Posts
         </h2>
         <p class="m-auto w-4/5 text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia assumenda quisquam, similique quam optio voluptatem nihil quae amet autem libero facilis vitae non nemo veniam unde, magnam nam, distinctio officia. Quo modi laboriosam officia optio nam distinctio odio ducimus autem, deleniti, ipsa inventore quisquam facilis non exercitationem quibusdam quia numquam!
+            Enjoy the blogs and learn what you feel and experience what you think.
         </p>
     </div>
     <div class="sm:grid grid-cols-2 w-4/5 m-auto">
         <div class="flex bg-yellow-700 text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
                 <span class="uppercase text-xs">
-                    Php
+                    Laravel
                 </span>
                 <h3 class="text-xl font-bold py-10">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ea debitis cum, autem maxime rem laudantium dolorum illum, totam repudiandae voluptates exercitationem aperiam dolores ipsum deserunt distinctio nesciunt, atque suscipit?
+                   Laravel framework is one of the good framework which enable developer to create beautiful website with more complex functionality
                 </h3>
                 <a href="" class="uppercase bg-transparent border-2 border-gray-50 text-gray-100 text-xs font-extrabold py-3
                 px-5 rounded-3xl">Find out more</a>
